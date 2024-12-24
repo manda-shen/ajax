27681e1e0624878409db6d98eb7de11ee4d2ff07
Manda <mandashen@gmail.com>
2024-12-24 16:34:22 +0800

    ajax新增學生資料

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -1,5 +1,18 @@
 <?php
 //處理刪除資料的請求
+include_once "db.php";
+$id=$_POST['id'];
+
+// 後端作法
+// $row=$Stu->find($id);
+
+$Stu->del($id);
+// echo $Stu->del($id);
+
+echo $_POST['classroom'];
+
+// 後端作法
+// echo $row['classroom'];
 
 
 ?>
